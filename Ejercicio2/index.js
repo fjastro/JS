2e3b88623af8be2af89a1404e9851cb64a3ebbba
fjastro <fjcgcatasol@gmail.com>
2022-01-29 12:34:03 +0100

    Ejercicio2

---
+++ Ejercicio2/index.js
@@ -0,0 +1,14 @@
+// Mi cumpleaños es el 2 de Diciembre
+
+const persona = {
+    day: 2,
+    month: 'Diciembre'
+}
+
+//prueba
+console.log(persona.day);
+
+
+//cumpleaños
+const cumpleaños = ("Mi cumpleaños es el" + persona.day);
+console.log(cumpleaños);
