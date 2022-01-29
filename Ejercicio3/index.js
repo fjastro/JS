901efb28b37c2619e36e9adf08aae5fd69bdaab2
fjastro <fjcgcatasol@gmail.com>
2022-01-29 20:09:47 +0100

    Ejercicio3

--- Ejercicio3/index.js
+++ Ejercicio3/index.js
@@ -5,5 +5,33 @@ const data = ["hola", 2, 5, "adios"];
 
 //Número mayor
 
-comp = data [1]> data [2];
+comp = data [1] > data [2];
 console.log(comp);
+comp2 = data [1] < data [2];
+console.log(comp2);
+
+//Suma
+
+var suma;
+suma = data [1] + data [2];
+console.log('suma', suma);
+
+//Resta
+var Resta;
+Resta = data [1] - data [2];
+console.log('Resta', Resta);
+
+//Producto
+var Producto;
+Producto = data [1] * data [2];
+console.log('Producto', Producto);
+
+//Cociente
+var Cociente
+Cociente = data [1] / data [2];
+console.log('Cociente', Cociente);
+
+//Resto
+var Resto
+Resto = data [1] % data [2];
+console.log('Resto', Resto)
