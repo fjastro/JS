2a718575d471360ee558d9368780ff9c640a7a03
fjastro <fjcgcatasol@gmail.com>
2022-01-31 17:22:56 +0100

    Ejercicio4

---
+++ Ejercicio4/index.js
@@ -0,0 +1,14 @@
+//Uso de If
+
+const numero1 = 5;
+const numero2 = 8;
+
+if (numero1<=numero2) {
+    alert (numero1 + " no es mayor que " + numero2)
+};
+console.log('5 no es mayor que 8')
+
+
+if (numero1-6) {
+    alert (numero1 + " es negativo o distinto de cero ")
+};
