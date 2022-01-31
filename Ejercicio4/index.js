76a329e154566a8d4389d5785ce17ad641decfe7
fjastro <fjcgcatasol@gmail.com>
2022-02-01 00:08:21 +0100

    Ejercicio4

--- Ejercicio4/index.js
+++ Ejercicio4/index.js
@@ -3,16 +3,19 @@
 const numero1 = 5;
 const numero2 = 8;
 
-if (numero1<=numero2) {
-    alert (numero1 + " no es mayor que " + numero2)
+if (numero1 <= numero2) {
+    alert ("numero1 no es mayor que numero2")
 };
-console.log('5 no es mayor que 8');
 
 
-if (numero2>0) {
-    alert (numero2 + " es positivo ")
+
+if (numero2 >= 0) {
+    alert ("numero2 es positivo")
 };
-console.log(numero2 + " es positivo ");
+
+
+
+
 
 
 
