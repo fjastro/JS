f06d43a10a30b2931123b49b3ca9b0b1517c2c46
fjastro <fjcgcatasol@gmail.com>
2022-02-06 19:14:04 +0100

    Ejercicio13

---
+++ Ejercicio13/index.js
@@ -0,0 +1,23 @@
+const users = [
+    {username: 'ppc90' , age: 30, premium: false},
+    {username: 'lu65' , age: 24, premium: false},
+    {username: 'maria3' , age: 36, premium: false},
+    {username: 'abc123' , age: 20, premium: true},
+    {username: 'sergio58' , age: 26, premium: true},
+];
+
+
+
+
+const userpremium = users
+    .filter(function(user){
+        return user.premium == true
+    });
+
+
+const mostrarMensaje = premium
+if (user = true) {
+    alert ("premium")
+};
+    
+console.log(mostrarMensaje)
